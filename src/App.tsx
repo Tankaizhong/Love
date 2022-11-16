e9b44d8bcc87f41d38a768878e972554eb43fef7
Tankaizhong <2534658839@qq.com>
2022-11-16 17:06:53 +0800

    complate

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.less";
+function App() {
+  return (
+    <div className="App">
+      <div className="box"></div>
+      <div className="box"></div>
+      <div className="box"></div>
+    </div>
+  );
+}
+
+export default App;
